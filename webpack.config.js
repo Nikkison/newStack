ba4c7506a11d687ff4d57c0df29bafc2e10a3837
狩野 紀彦 <m-karino@protosolution.co.jp>
2017-10-27 21:50:44 +0900

    webpack設定

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: './src/app.js',
+  // ファイルの出力設定
+  output: {
+    //  出力ファイルのディレクトリ名
+    path: `${__dirname}/build`,
+    // 出力ファイル名
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        // 拡張子 .js の場合
+        test: /\.js$/,
+        use: [
+          {
+            // Babel を利用する
+            loader: 'babel-loader',
+            // Babel のオプションを指定する
+            options: {
+              presets: [
+                // env を指定することで、ES2017 を ES5 に変換。
+                ['env', { 'modules': false }]
+              ]
+            }
+          }
+        ],
+        // node_modules は除外する
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  // ソースマップを有効にする
+  // 開発ツールでwebpackのところからデバックできる
+  devtool: 'source-map'
+};
